d28e19955a5311d3035f6b2bba5cef6cff10b9d1
Tanki <mjrcoetzee+1@outlook.com>
2023-11-13 10:57:45 +0200

    initial commit for addons

---
+++ rework/addonLoader.ts
@@ -0,0 +1,52 @@
+import fs from "fs";
+
+export type resolver = {
+    name: string;
+    regexMatches: RegExp[];
+    resolve: (url: string) => Promise<string>;
+}
+
+export type patch = {
+    prefix: {
+        
+    } | undefined;
+    postfix: {
+
+    } | undefined;
+    name: string;
+}
+
+export type addon = {
+    name: string;
+    description: string;
+    version: string;
+    sources: string[];
+    credits: string;
+    resolvers: resolver[];
+    private: boolean;
+    type: "songResolver";
+} | {
+    name: string;
+    description: string;
+    version: string;
+    sources: string[];
+    credits: string;
+    patches: patch[];
+    private: boolean;
+    type: "patch";
+}
+
+export default class addonLoader {
+    private addonPath: string;
+    private addons: addon[] = [];
+    constructor(addonPath: string) {
+        this.addonPath = addonPath;
+    }
+
+    async readAddons() {
+        for (const addon of fs.readdirSync(this.addonPath)) {
+            const addonInfo: addon = await import(`${this.addonPath}/${addon}`).then((m) => m.default);
+            this.addons.push(addonInfo);
+        }
+    }
+}
