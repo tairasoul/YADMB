c744fc5f0a39104708b556e89f49ce7cee0aadaa
Tanki <mjrcoetzee+1@outlook.com>
2023-11-13 13:43:26 +0200

    alr it work

--- dist/addonTest.js
+++
@@ -1,7 +0,0 @@
-import addonloader from "./addonLoader.js";
-import path from 'path';
-import { fileURLToPath } from 'url';
-const __dirname = path.dirname(decodeURIComponent(fileURLToPath(import.meta.url)));
-const loader = new addonloader(`${__dirname}/addons`);
-await loader.readAddons();
-console.log(loader.addons);
